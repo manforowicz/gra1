5d77db22c3fa355d28ac4641c2571f33b098eea0
manforowicz <manforowicz@gmail.com>
2017-01-06 20:20:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+hero = {
+  x: 50,
+  y: 50
+}
+
+addEventListener("keydown", function(event) {
+	if (event.keyCode == 39) {
+    x = x + 10;
+  }
+});
+
 var main = function(canvas) {
   canvas.fillStyle = "red";
-  canvas.fillRect(10, 10, 40, 30);
+  canvas.fillRect(x, y, 40, 40);
+  requestAnimationFrame(function() {
+    main(canvas);
+  });
 }
